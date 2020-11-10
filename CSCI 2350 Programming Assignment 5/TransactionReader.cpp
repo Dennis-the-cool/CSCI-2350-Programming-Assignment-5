b7524a587bd665bb118e1fef0dff9837e7aa21fc
Dennis-the-cool <BwMfkMGe5Rx5t7F>
2020-11-10 09:00:11 -0500

    Created base file reading

--- CSCI 2350 Programming Assignment 5/TransactionReader.cpp
+++ CSCI 2350 Programming Assignment 5/TransactionReader.cpp
@@ -1,3 +1,18 @@
-//Test commit
-//Test Commit 2
-//Testing 3
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	ifstream file;
+	file.open("TestTransactionFile.txt");
+	string line;
+	while (getline(file, line))
+	{
+		cout << line << endl;
+	}
+	file.close();
+	return 0;
+}
+
