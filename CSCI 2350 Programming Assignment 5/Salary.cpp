44746f615973d3c7ccdcd351ee31c318180978e0
Jasper Strickland <jasperstrickland@Jaspers-MacBook-Pro.local>
2020-11-12 09:14:48 -0500

    test

--- CSCI 2350 Programming Assignment 5/Salary.cpp
+++ CSCI 2350 Programming Assignment 5/Salary.cpp
@@ -1,1 +1,26 @@
-// Test
+#include "Salary.h"
+
+Salary:: Salary()
+{
+
+}
+
+Salary:: ~Salary()
+{
+
+}
+
+void Salary:: payroll()
+{
+
+}
+
+void Salary:: hire(int, string, string, double, int)
+{
+
+}
+
+void Salary:: termination()
+{
+
+} 
--- CSCI 2350 Programming Assignment 5/Salary.h
+++ CSCI 2350 Programming Assignment 5/Salary.h
@@ -1,1 +1,15 @@
-// Test
+#pragma once
+#include <string>
+using namespace std; 
+
+class Salary
+{
+private:
+    int dayFired;
+public:
+    Salary();
+    ~Salary();
+    void payroll();
+    void hire(int, string, string, double, int);
+    void termination();
+};
