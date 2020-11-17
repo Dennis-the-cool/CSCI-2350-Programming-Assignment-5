c9e7b199747a20318f16c17f89d00c02fa06e45f
Jasper Strickland <jasperstrickland@Jaspers-MacBook-Pro.local>
2020-11-17 09:07:06 -0500

    test salary payroll

--- CSCI 2350 Programming Assignment 5/Employee.cpp
+++ CSCI 2350 Programming Assignment 5/Employee.cpp
@@ -32,7 +32,30 @@ Salary:: ~Salary()
 
 void Salary:: payroll()
 {
+    double totalPay;
+    
+    if(hiredDate == 1 && fired == false)
+    {
+        totalPay = payRate;
+    }
+    else
+    {
+        double dailyPay = payRate/30;
+        totalPay = dailyPay * (dayFired - hiredDate + 1);
+    }
 
+    char* separate = ",";
+    
+    // I think that opening this file in append, rather than write, may help write on a newline each time.
+    FILE* outputFile = fopen("outputFile.txt", "a");
+    
+    fwrite(&id, sizeof(int), 1, outputFile);
+    fwrite(separate, sizeof(char), 1, outputFile);
+    fwrite(&name, sizeof(string), 1, outputFile);
+    fwrite(separate, sizeof(char), 1, outputFile);
+    fwrite(&totalPay, sizeof(double), 1, outputFile);
+
+    fclose(outputFile);    
 }
 
 Hourly:: Hourly()
